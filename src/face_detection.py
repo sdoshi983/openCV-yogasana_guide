b5ba87155500a436241d5f26c8cc61ab9d6dcfad
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-25 18:50:42 +0530

    Face detection completed

--- src/face_detection.py
+++ src/face_detection.py
@@ -1,16 +1,21 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-classifer = cv2.CascadeClassifier("assets/haarcascade_frontalface_default.xml")
+classifier = cv2.CascadeClassifier("../assets/haarcascade_frontalface_default.xml")
 
 while True:
     isReturned, frame = cap.read()
     if isReturned:
-        cv2.imshow('My Face', frame)
+        faces = classifier.detectMultiScale(frame)
 
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
+        for face in faces:
+            x, y, w, h = face
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 5)
+
+        cv2.imshow('My Face', frame)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
