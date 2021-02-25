174f82a4c6e48935dbb26486c94c9517a95909d5
sdoshi983 <sdoshi983@rku.ac.in>
2021-02-25 18:10:21 +0530

    Code for web cam completed

--- src/face_detection.py
+++ src/face_detection.py
@@ -2,4 +2,12 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+while True:
+    isReturned, frame = cap.read()
+    if isReturned:
+        cv2.imshow('My Face', frame)
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
 
